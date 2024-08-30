78aded6d737ea244d1e124a1b378914ca3e954f6
GastonGit <73943682+GastonGit@users.noreply.github.com>
2024-08-30 17:10:57 +0200

    fix: improve error handling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 dotenv.config();
@@ -8,10 +8,35 @@ import routes from "./routes";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+/**
+ * Applications
+ */
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use("/api", routes);
 
+/**
+ * Error handling
+ */
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+});
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  process.exit(1);
+});
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
+
+/**
+ * Start the server
+ */
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

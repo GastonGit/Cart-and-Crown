6471508e9f0692c7edff80278a256777bd51c6e8
GastonGit <73943682+GastonGit@users.noreply.github.com>
2024-08-30 15:09:12 +0200

    fix: remove duplicate file and setup morgan

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 dotenv.config();
 
 import routes from "./routes";
-import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use("/api", routes);
 

2f75d02fea708f87836b2ede9d9b3147f1dc481b
GastonGit <73943682+GastonGit@users.noreply.github.com>
2024-09-02 18:13:07 +0200

    Add lint (#4)
    * fix: move dependencies to devDependencies
    
    * feat: implement linting and fix lint

--- backend/src/functions/isAuthenticated.ts
+++ backend/src/functions/isAuthenticated.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
-export const handler = (req: Request, res: Response) => {
+export function handler(req: Request, res: Response) {
   res.send(`True`);
-};
+  return;
+}
--- backend/src/util/auth.ts
+++ backend/src/util/auth.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 
 interface UserPayload {
   id: string;

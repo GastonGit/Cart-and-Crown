be855b35d2a15a0a1ba98c77140a6a4ec8530054
GastonGit <73943682+GastonGit@users.noreply.github.com>
2024-09-19 16:21:01 +0200

    refactor: cleanup securePassword

--- backend/src/domain/user/securePassword.ts
+++ backend/src/domain/user/securePassword.ts
@@ -1,7 +1,7 @@
 import * as bcrypt from "bcryptjs";
 
+const SALT_ROUNDS = 10;
+
 export async function securePassword(password: string) {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
+  return await bcrypt.hash(password, SALT_ROUNDS);
 }

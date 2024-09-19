7dd85c2573d6fc8362e21df21fc949da6befcf49
GastonGit <73943682+GastonGit@users.noreply.github.com>
2024-09-19 16:23:44 +0200

    refactor: move models folder to db

--- backend/src/db/queries/user.ts
+++ backend/src/db/queries/user.ts
@@ -1,4 +1,4 @@
-import { User } from "../../models/user";
+import { User } from "../models/user";
 
 async function createUser(user: { username: string; passwordHash: string }) {
   return await User.create({
